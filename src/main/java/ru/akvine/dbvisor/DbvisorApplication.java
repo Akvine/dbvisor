ab62553764ac7dbe25422c603d88ee7a91746924
akvine <redgooseman55@gmail.com>
2025-02-22 15:14:52 +0300

    init commit

---
+++ src/main/java/ru/akvine/dbvisor/DbvisorApplication.java
@@ -0,0 +1,13 @@
+package ru.akvine.dbvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akvine/dbvisor/DbvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.dbvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
