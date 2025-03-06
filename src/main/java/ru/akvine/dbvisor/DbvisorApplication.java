9ab86435f45d916c21c409523f451cc2c8c3ab65
akvine <redgooseman55@gmail.com>
2025-03-06 22:06:13 +0300

    add spring cloud client to project

--- src/main/java/ru/akvine/dbvisor/DbvisorApplication.java
+++ src/main/java/ru/akvine/dbvisor/DbvisorApplication.java
@@ -2,8 +2,10 @@ package ru.akvine.dbvisor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DbvisorApplication {
 
 	public static void main(String[] args) {
